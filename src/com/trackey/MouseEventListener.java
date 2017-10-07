0ce21b368a21a98c9577d118f1c8c973cbb352c3
Lucy Firman <lucyf@stormbit.net>
2017-10-07 13:21:09 +0100

    fuck

--- src/com/trackey/MouseEventListener.java
+++ src/com/trackey/MouseEventListener.java
@@ -1,43 +1,14 @@
 package com.trackey;
 
+import java.awt.*;
+
 // import NativeHook libraries
+public class MouseEventListener {
 
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
-import org.jnativehook.mouse.NativeMouseEvent;
-import org.jnativehook.mouse.NativeMouseInputListener;
+    Point mouseLocation;
 
-public class MouseEventListener implements NativeMouseInputListener {
 
-    public void nativeMouseClicked(NativeMouseEvent e){}
 
-    public void nativeMousePressed(NativeMouseEvent e){}
 
-    public void nativeMouseReleased(NativeMouseEvent e){}
 
-    public void nativeMouseDragged(NativeMouseEvent e){}
-
-    public void nativeMouseMoved(NativeMouseEvent e){
-        // logic for onMove operations goes here.
-        System.out.println("Mouse position: " + e.getX() + e.getY());
-    }
-
-public static void main(String args[]) {
-        try {
-            GlobalScreen.registerNativeHook();
         }
-        catch (NativeHookException ex){
-            System.out.println(ex.getMessage());
-        }
-
-        System.exit(1);
-
-        // Construct the example object.
-        MouseEventListener example = new MouseEventListener();
-
-        // Add the appropriate listeners.
-        GlobalScreen.addNativeMouseListener(example);
-        GlobalScreen.addNativeMouseMotionListener(example);
-    }
-
-}
